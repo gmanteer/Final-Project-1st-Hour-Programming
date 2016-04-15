a6705a761eab46ca1dc16d8ff9c8e819143c6ba3
gmanteer <grantpolmanteer@gmail.com>
2016-04-15 08:10:08 -0400

    First commit

---
+++ Final Project/GitHub.cpp
@@ -0,0 +1,6 @@
+echo "# Final-Project-1st-Hour-Programming" >> README.md
+git init
+git add README.md
+git commit - m "first commit"
+git remote add origin https ://github.com/gmanteer/Final-Project-1st-Hour-Programming.git
+git push - u origin master
